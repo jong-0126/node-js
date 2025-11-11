b349deb5318b36acac6bd99019c09ffb81f70888
baegjonghyeon <baek011226@naver.com>
2025-11-11 22:21:41 +0900

    first commit

---
+++ hello.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.end('Hello Node.js');
+});
+
+server.listen(3000, () => console.log('Server running on 3000'));
