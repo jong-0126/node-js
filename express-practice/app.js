8c10f85e7982cc7a223ef05ed31fe1ef0ffb0aed
baegjonghyeon <baek011226@naver.com>
2025-11-13 19:47:19 +0900

    feat:express

--- express-practice/app.js
+++ express-practice/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('홈페이지 입니다.');
 });
+
+app.get('/about', (rea, res) => {
+    res.send('소개 페이지 입니다.');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('문의 페이지 입니다.')
+})
 
 app.listen(3000, () => {
     console.log('Express Server Running');
